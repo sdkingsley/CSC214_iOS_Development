e29e489cdeb8d1b66c7e511fb8152feba79ac38a
sdkingsley <skingsl2@u.rochester.edu>
2016-11-02 17:59:52 -0400

    Finished App

--- Assignment7/Assignment7/MakeupViewController.swift
+++ Assignment7/Assignment7/MakeupViewController.swift
@@ -30,17 +30,49 @@ class MakeupViewController: UITableViewController {
         
         makeup = Makeup()
         
-        for _ in 0..<5 {
-            _ = makeup.addRandomFace()
-            _ = makeup.addRandomLips()
-            _ = makeup.addRandomEyes()
-        }
+//        for _ in 0..<5 {
+//            _ = makeup.addRandomFace()
+//            _ = makeup.addRandomLips()
+//            _ = makeup.addRandomEyes()
+//        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func addFace(_ sender: AnyObject) {
+        if let index = makeup.addRandomFace() {
+            let indexPath = NSIndexPath(row: index, section: FACE)
+            tableView.insertRows(at: [indexPath as IndexPath], with: .automatic)
+        }
+    }
+    
+    @IBAction func addLip(_ sender: AnyObject) {
+        if let index = makeup.addRandomLips() {
+            let indexPath = NSIndexPath(row: index, section: LIPS)
+            tableView.insertRows(at: [indexPath as IndexPath], with: .automatic)
+        }
+    }
+    
+    @IBAction func addEye(_ sender: AnyObject) {
+        if let index = makeup.addRandomEyes() {
+            let indexPath = NSIndexPath(row: index, section: EYES)
+            tableView.insertRows(at: [indexPath as IndexPath], with: .automatic)
+        }
+    }
+    
+    @IBAction func edit(_ sender: UIButton) {
+        if isEditing == false {
+            setEditing(true, animated: true)//turns the thing on if its off
+            sender.setTitle("Done", for: .normal)
+        }
+        else {
+            setEditing(false, animated: true)//turns the the off if its on
+            sender.setTitle("Edit", for: .normal)
+        }
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -100,7 +132,63 @@ class MakeupViewController: UITableViewController {
         
         return cell
     }
+    
+    func deleteRow(_ path: IndexPath) {
+        switch(path.section) {
+        case FACE:
+            let face = makeup.faces[path.row]
+            verifyDelete(face.kind, {
+                (action) -> Void in
+                self.makeup.removeFace(face)
+                self.tableView.deleteRows(at: [path], with: .automatic)
+            })
+        case LIPS:
+            let lips = makeup.listlips[path.row]
+            verifyDelete(lips.kind, {
+                (action) -> Void in
+                self.makeup.removeLips(lips)
+                self.tableView.deleteRows(at: [path], with: .automatic)
+            })
+        case EYES:
+            let eyes = makeup.listeyes[path.row]
+            verifyDelete(eyes.kind, {
+                (action) -> Void in
+                self.makeup.removeEyes(eyes)
+                self.tableView.deleteRows(at: [path], with: .automatic)
+            })
+        default:
+            break
+        }
+    }
+    
+    func verifyDelete(_ name: String, _ delete: @escaping (UIAlertAction) -> Void) {
+        let title = "Delete \(name)?"
+        let message = "Are you sure that you want to delete this item?"
+        
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
+        
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        ac.addAction(cancelAction)
+        
+        let deleteAction = UIAlertAction(title: "Delete", style: .destructive, handler: delete)
+        ac.addAction(deleteAction)
+        
+        present(ac, animated: true, completion: nil)
+    }
 
+    func moveRow(_ from: IndexPath, _ to: IndexPath) {
+        switch(from.section) {
+        case FACE:
+            makeup.moveFace(from.row, to.row)
+        case LIPS:
+            makeup.moveLips(from.row, to.row)
+        case EYES:
+            makeup.moveEyes(from.row, to.row)
+        default:
+            break
+        }
+    }
 
 
     //
@@ -119,15 +207,15 @@ class MakeupViewController: UITableViewController {
         return getTableCell(indexPath)
     }
     
-//    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-//        if editingStyle == .delete {
-//            deleteRow(indexPath)
-//        }
-//    }
-//    
-//    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
-//        moveRow(sourceIndexPath, destinationIndexPath)
-//    }
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            deleteRow(indexPath)
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        moveRow(sourceIndexPath, destinationIndexPath)
+    }
 
 
 }
